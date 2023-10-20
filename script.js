ba437f18d3755cf7aea46777afdbe5e6927fc595
Marcus-Aastum <marcusaastum@gmail.com>
2023-10-20 13:12:29 +0200

    Add tasklist to document
    Create a JS function that recusivly adds all elements in the tasklist. Also made the list items into objects for futher functionality down the road
    
    Create list in html #5

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 tasklist = [];
 function createTaskList(textContent){
-    tasklist.push(textContent)
+    tasklist.push({id: tasklist.length, text: textContent, checked: false});
+    console.log(tasklist)
+    updateTasks()
+}
+function updateTasks(){
+    taskListElement = document.getElementById("taskList");
+    taskListElement.innerHTML = "";
+    
+    for (let index = 0; index < tasklist.length; index++) {
+        listElement = document.createElement("li");
+        listElement.id = tasklist[index].id;
+        contentText = document.createTextNode(tasklist[index].text);
+        listElement.appendChild(contentText);
+        taskListElement.appendChild(listElement);        
+    }
 }
