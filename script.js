e7e96e83ed835638a5de056c96b4136b21190bc7
Marcus-Aastum <marcusaastum@gmail.com>
2023-10-21 22:16:57 +0200

    Cross out items
    Added a checkbox that crosses out items when they have been done
    
    Ability to check off items #6 Create list in html #5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-tasklist = [];
+let tasklist = [];
 function createTaskList(textContent){
     tasklist.push({id: tasklist.length, text: textContent, checked: false});
     console.log(tasklist)
@@ -7,12 +7,29 @@ function createTaskList(textContent){
 function updateTasks(){
     taskListElement = document.getElementById("taskList");
     taskListElement.innerHTML = "";
-    
+
     for (let index = 0; index < tasklist.length; index++) {
-        listElement = document.createElement("li");
-        listElement.id = tasklist[index].id;
-        contentText = document.createTextNode(tasklist[index].text);
+        let listElement = document.createElement("li");
+        listElement.id = "task_" + tasklist[index].id;
+        let contentText = document.createTextNode(tasklist[index].text);
         listElement.appendChild(contentText);
+
+        let checkbox = document.createElement("input"); 
+        checkbox.type = "checkbox";
+        checkbox.setAttribute("onchange", "checkboxChange(this)");
+        checkbox.id = "checkbox_" + tasklist[index].id;
+        if(tasklist[index].checked){
+            checkbox.checked = true;
+            listElement.style.textDecoration = "line-through";
+        }
+        else{
+            listElement.style.textDecoration = "none";
+        }
+        listElement.appendChild(checkbox);
         taskListElement.appendChild(listElement);        
     }
+}
+function checkboxChange(checkbox){
+    tasklist[checkbox.id.split("_")[1]].checked = checkbox.checked;
+    updateTasks();
 }
