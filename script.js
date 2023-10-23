3fbd77f3e8cd9db5eb2d010d4b2df9ab96e11a71
Marcus-Aastum <marcusaastum@gmail.com>
2023-10-23 08:55:59 +0200

    Add delete button
    Add a delete button to delete tasks
    
    Add delete button #8

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function updateTasks(){
     taskListElement.innerHTML = "";
 
     for (let index = 0; index < tasklist.length; index++) {
+        if (!tasklist[index]){
+            continue;
+        }
         let listElement = document.createElement("li");
         listElement.id = "task_" + tasklist[index].id;
         let contentText = document.createTextNode(tasklist[index].text);
@@ -18,6 +21,11 @@ function updateTasks(){
         checkbox.type = "checkbox";
         checkbox.setAttribute("onchange", "checkboxChange(this)");
         checkbox.id = "checkbox_" + tasklist[index].id;
+
+        let deletebutton = document.createElement("button");
+        let deleteText = document.createTextNode("Delete");
+        deletebutton.setAttribute("onclick", "deleteTask(this)");
+        deletebutton.appendChild(deleteText);
         if(tasklist[index].checked){
             checkbox.checked = true;
             listElement.style.textDecoration = "line-through";
@@ -26,10 +34,23 @@ function updateTasks(){
             listElement.style.textDecoration = "none";
         }
         listElement.appendChild(checkbox);
+        listElement.appendChild(deletebutton);
         taskListElement.appendChild(listElement);        
     }
 }
 function checkboxChange(checkbox){
     tasklist[checkbox.id.split("_")[1]].checked = checkbox.checked;
+    updateTasks();  
+}
+function deleteTask(button){
+    for (let index = 0; index < tasklist.length; index++) {
+        if (!tasklist[index]){
+            continue;
+        }
+        if (tasklist[index].id == button.parentNode.id.split("_")[1]){
+            delete tasklist[index];
+            break;
+        }
+    }
     updateTasks();
 }
